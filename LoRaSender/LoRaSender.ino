8162b237ca74f883f5d06eb5f06a4553ec8f870e
saulompg <saulo.mpg@hotmail.com>
2024-06-25 14:33:37 -0300

    LoRa Sender code

---
+++ LoRaSender/LoRaSender.ino
@@ -0,0 +1,61 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+/* 
+  CONEXÃO LoRa RA-02:ESP8266
+  NSS   GPIO15 - D8
+  MOSI  GPIO13 - D7
+  MISO  GPIO12 - D6
+  SCK   GPIO14 - D5
+  RST   GPIO2  - D4
+  DIO0  GPIO5  - D1
+*/
+
+const int ssPin = 15;   // NSS
+const int rstPin = 2;   // RST
+const int dio0Pin = 5;  // DIO0
+const int ledPin = 4;   // LED
+int SyncWord = 0x22;
+int state = 0;
+int counter = 0;
+
+void setup() {
+  // Inicia comunicação Serial
+  Serial.begin(115200);
+  // Define D2 (GPIO4) como saída
+  pinMode(ledPin, OUTPUT);
+
+  // Define configuração LoRa
+  LoRa.setPins(ssPin, rstPin, dio0Pin);
+  if (!LoRa.begin(433E6)) {
+    Serial.println("LoRa falhou");
+    while (1);
+  }
+  LoRa.setTxPower(20);
+  LoRa.setSpreadingFactor(12);
+  LoRa.setSignalBandwidth(62.5E3);
+  LoRa.setCodingRate4(8);
+  LoRa.setSyncWord(SyncWord);
+  
+  delay(5000);
+
+  Serial.println("\nLoRa iniciado");
+}
+
+void loop() {
+
+  Serial.print("Enviando pacote: ");
+  Serial.println(counter);
+
+  // send packet
+  LoRa.beginPacket();
+  LoRa.print("hello LoRa ");
+  LoRa.print(counter);
+  state = !state;
+  digitalWrite(4, state);
+  LoRa.endPacket();
+
+  counter++;
+
+  delay(2000);
+}
