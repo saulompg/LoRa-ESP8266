f0bca57903dad0b9c275a0064258818e3d67ca0f
saulompg <saulo.mpg@hotmail.com>
2024-07-16 13:06:30 -0300

    refactoring

--- esp8266_master/esp8266_master.ino
+++ esp8266_master/esp8266_master.ino
@@ -1,28 +1,30 @@
-// -------------------------------------------------------------------------- DIRETIVAS DE PRÉ-PROCESSAMENTO
+// --------------------------------------------------------------------- DIRETIVAS DE PRÉ-PROCESSAMENTO
 #include <SPI.h>
 #include <LoRa.h>
-#include <string>
 #include <WiFiManager.h>
 #include <PubSubClient.h>
 
-#define LED_BUILTIN 2
-
-// -------------------------------------------------------------------------- VARIÁVEIS AUXILIARES
-// -------------------------------------------------------------------------- WIFI
-IPAddress ip_esp(192,168,1,11);
+// --------------------------------------------------------------------- VARIÁVEIS AUXILIARES
+// --------------------------------------------------------------------- WIFI
+IPAddress ip_esp(192,168,1,20);
 IPAddress gateway(192,168,1,1);
 IPAddress mask(255,255,255,0);
-// -------------------------------------------------------------------------- MQTT
+// --------------------------------------------------------------------- MQTT
 IPAddress mqtt_server(192, 168, 1, 10);
 const int mqtt_port = 1883;
 const char* mqtt_user = "esp_mqtt";
 const char* mqtt_password = "4u70m4c40ESP";
-const char* topic_master_available = "homeassistant/master/available";
-const char* topic_slave_led_set = "homeassistant/master/slave/led/set";
-const char* topic_slave_led_state = "homeassistant/master/slave/led/state";
 WiFiClient esp_client;
 PubSubClient client(esp_client);
-// -------------------------------------------------------------------------- CONFIGURAÇÃO LoRa GPIO
+// --------------------------------------------------------------------- TÓPICOS AVAILABLE
+const char* topic_esp_available = "homeassistant/master/available";
+// --------------------------------------------------------------------- TÓPICOS SUBSCRIBER
+const char* topic_led_set = "homeassistant/slave/led/set";
+const char* topic_switch1_set = "homeassistant/slave/switch/1/set";
+// --------------------------------------------------------------------- TÓPICOS PUBLISHER
+const char* topic_led_state = "homeassistant/slave/led/state";
+const char* topic_switch1_state = "homeassistant/slave/switch/1/state";
+// --------------------------------------------------------------------- CONFIGURAÇÃO LoRa GPIO
 /* 
   CONEXÃO LoRa RA-02:ESP8266
   NSS   GPIO15 - D8
@@ -35,27 +37,21 @@ PubSubClient client(esp_client);
 const int ss = 15;
 const int rst = 4;
 const int dio0 = 5;
-// -------------------------------------------------------------------------- MENSAGEM
-volatile bool dataReceived = false;                                        // Acusa o recebimento de dados
-String incoming;
-String outgoing;                                                           // String de saída
-byte msgCount = 0;                                                         // ID da mensagem
-byte localAddress = 0x01;                                                  // endereço deste dispositivo
-byte slave1Address = 0x02;                                                 // endereço do slave 1
-byte slave2Address = 0x03;                                                 // endereço do slave 2
-byte broadcast = 0xFF;                                                     // endereço broadcast
-byte slv = 0;                                                              // escravo selecionado
-byte led_state = 0x00;
-// -------------------------------------------------------------------------- TIMER
+// --------------------------------------------------------------------- MENSAGEM
+volatile bool dataReceived = false;                                     // Acusa o recebimento de dados
+byte localAddress = 0x01;                                               // endereço deste dispositivo
+byte slaveAddress = 0x02;                                               // endereço do slave 1
+byte broadcast = 0xFF;                                                  // endereço broadcast
+// --------------------------------------------------------------------- TIMER
 unsigned long lastSendTime = 0;
-const int interval = 5000;
+const int interval = 30000;
 
-// -------------------------------------------------------------------------- DECLARAÇÃO DAS FUNÇÕES
-// -------------------------------------------------------------------------- CONFIGURAÇÃO WiFi
+// --------------------------------------------------------------------- DECLARAÇÃO DAS FUNÇÕES
+// --------------------------------------------------------------------- CONFIGURAÇÃO WiFi
 void setupWiFi(void) {
   WiFiManager wm;
-  // wm.setSTAStaticIPConfig(ip_esp, gateway, mask);
-  bool res = wm.autoConnect("ESP_MASTER", "automacaoESP32");
+  wm.setSTAStaticIPConfig(ip_esp, gateway, mask);
+  bool res = wm.autoConnect("ESP_MASTER", "automacaoESP");
   
   if (!res) {
     Serial.println("Falha ao conectar");
@@ -66,14 +62,21 @@ void setupWiFi(void) {
   }
 }
 
-// -------------------------------------------------------------------------- CONEXÃO MQTT
+// --------------------------------------------------------------------- CONFIGURAÇÃO MQTT
+void setupMQTT(void) {
+  client.setServer(mqtt_server, mqtt_port);                           // Define endereço do servidor e porta
+  client.setCallback(callback);                                       // Define função callback
+}
+
+// --------------------------------------------------------------------- CONEXÃO MQTT
 void connectMQTT(void) {
   while(!client.connected()) {
     Serial.print("Estabelecendo conexão com o servidor: ");
     if(client.connect("ESP8266Client", mqtt_user, mqtt_password)) {
       Serial.println("Conectado!");
-      client.publish(topic_master_available, "online");
-      client.subscribe(topic_slave_led_set);
+      client.publish(topic_esp_available, "online");
+      client.subscribe(topic_led_set);
+      client.subscribe(topic_switch1_set);
     } else {
       Serial.print("Falha, rc=");
       Serial.print(client.state());
@@ -83,31 +86,44 @@ void connectMQTT(void) {
   }
 }
 
-// -------------------------------------------------------------------------- CALLBACK MQTT
+// --------------------------------------------------------------------- CALLBACK MQTT
 void callback(char* topic, byte* payload, unsigned int length) {
-  // Converte Array de Bytes em String e exibe conteúdo da mensagem
-  std::string incoming(reinterpret_cast<char const*>(payload), length);
-  Serial.print("Mensagem recebida [");
-  Serial.print(topic);
-  Serial.printf("] : %s\n", incoming.c_str());
+  // Converte Array de Bytes em String
+  String message;
+  for (unsigned int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
 
-  if(String(topic) == topic_slave_led_set) {
-    if (incoming == "ON") {
-      sendMessage(incoming.c_str(), slave1Address);
-    } else if (incoming == "OFF"){
-      sendMessage(incoming.c_str(), slave1Address);
-    }
+  // Exibe o tópico e a mensagem recebida
+  Serial.print("Recebido MQTT[");
+  Serial.print(topic);
+  Serial.print("]: ");
+  Serial.println(message);
+
+  // Identifica no tópico para quem o comando será enviado
+  String command;
+  if (String(topic) == topic_led_set) {
+    command = "LED:" + message;
+  } else if (String(topic) == topic_switch1_set) {
+    command = "SWITCH1:" + message;
+  }
+
+  // Envia o comando para o Slave via LoRa
+  if (!command.isEmpty()) {
+    Serial.print("Enviado LoRa[0x" + String(slaveAddress) + "]: ");
+    Serial.println(command);
+    sendMessage(command, slaveAddress);
   }
 }
 
-// -------------------------------------------------------------------------- CONFIGURAÇÃO LoRa
+// --------------------------------------------------------------------- CONFIGURAÇÃO LoRa
 void setupLoRa(long frequency) {
-  LoRa.setPins(ss, rst, dio0);                                             // Define configuração LoRa
-  while (!LoRa.begin(frequency)) {                                         // Inicializa a comunicação LoRa
+  LoRa.setPins(ss, rst, dio0);                                        // Define configuração LoRa
+  while (!LoRa.begin(frequency)) {                                    // Inicializa a comunicação LoRa
     Serial.println(".");
     delay(500);
   }
-
+  LoRa.onReceive(onReceive);                                          // Define o que será realizado ao receber uma mensagem LoRa
   // LoRa.setTxPower(20);
   // LoRa.setSpreadingFactor(12);
   // LoRa.setSignalBandwidth(62.5E3);
@@ -117,112 +133,107 @@ void setupLoRa(long frequency) {
   delay(5000);
 }
 
-// -------------------------------------------------------------------------- SUBROTINA PARA ENVIO DE DADOS
+// --------------------------------------------------------------------- SUBROTINA PARA ENVIO DE DADOS
 void sendMessage(String outgoing, byte destination) {
-  LoRa.beginPacket();                                                      // inicializa o envio dos pacotes
-  LoRa.write(destination);                                                 // adiciona o endereço de destino
-  LoRa.write(localAddress);                                                // adiciona o endereço do emissor
-  LoRa.write(msgCount);                                                    // adiciona ID da mensagem
-  LoRa.write(outgoing.length());                                           // adiciona comprimento do payload
-  LoRa.print(outgoing);                                                    // adiciona payload
-  LoRa.endPacket();                                                        // finaliza o envio dos pacotes
-  msgCount++;                                                              // incrementa ID da mensagem
+  LoRa.beginPacket();                                                 // inicializa o envio dos pacotes
+  LoRa.write(destination);                                            // adiciona o endereço de destino
+  LoRa.write(localAddress);                                           // adiciona o endereço do emissor
+  LoRa.write(outgoing.length());                                      // adiciona comprimento do payload
+  LoRa.print(outgoing);                                               // adiciona payload
+  LoRa.endPacket();                                                   // finaliza o envio dos pacotes
+  LoRa.receive();                                                     // Volta para o modo receive
 }
 
-// -------------------------------------------------------------------------- SUBROTINA PARA RECEBIMENTO DE DADOS
+// --------------------------------------------------------------------- SUBROTINA PARA RECEBIMENTO DE DADOS
 void onReceive(int packetSize) {
-  if (packetSize == 0) return;                                             // se não há pacotes, retorne
+  if (packetSize == 0) return;                                        // se não há pacotes, retorne
   dataReceived = true;
 }
 
-// -------------------------------------------------------------------------- SUBROTINA PARA PROCESSAR DADOS DA MENSAGEM RECEBIDA
-void showData(void) {
-  // ------------------------------------------------------------------------ LENDO BYTES DO CABEÇALHO DO PACOTE
-  int recipient = LoRa.read();                                             // endereço do destinatário
-  byte sender = LoRa.read();                                               // endereço do remetente
-  byte incomingMsgId = LoRa.read();                                        // ID da mensagem
-  byte incomingLength = LoRa.read();                                       // comprimento da mensagem
-  // ------------------------------------------------------------------------ PERCORRE CONTEÚDO DA MENSAGEM E ARMAZENA OS DADOS
-  incoming = "";
-  while (LoRa.available()) {
-    incoming += (char)LoRa.read();
-  } 
-  // ------------------------------------------------------------------------ VERIFICA SE O COMPRIMENTO RECEBIDO CORRESPONDE AO ENVIADO
-  if (incomingLength != incoming.length()) {
-    Serial.println("erro: tamanho da mensagem não corresponde\n");
-    return;
-  }
-  // ------------------------------------------------------------------------ VERIFICA SE A MENSAGEM É ENCAMINHADA A ESTE DESTINATÁRIO
+// --------------------------------------------------------------------- SUBROTINA PARA PROCESSAR DADOS DA MENSAGEM RECEBIDA
+void receiveData(void) {
+  // ------------------------------------------------------------------- LENDO BYTES DO CABEÇALHO DO PACOTE
+  int recipient = LoRa.read();                                        // endereço do destinatário
+  byte sender = LoRa.read();                                          // endereço do remetente
+  byte incomingLength = LoRa.read();                                  // comprimento da mensagem
+  // ------------------------------------------------------------------- VERIFICA SE A MENSAGEM É ENCAMINHADA A ESTE DESTINATÁRIO
   if(recipient != localAddress && recipient != 0xFF) {
     Serial.println("Essa mensagem não é pra mim.\n");
     return;
   }
-  // ------------------------------------------------------------------------ EXIBE CONTEÚDO DA MENSAGEM
-  Serial.println("Recebido de: 0x" + String(sender, HEX));
+  // ------------------------------------------------------------------- PERCORRE CONTEÚDO DA MENSAGEM E ARMAZENA OS DADOS
+  String incoming;
+  while (LoRa.available()) {
+    incoming += (char)LoRa.read();
+  } 
+  // ------------------------------------------------------------------- VERIFICA SE O COMPRIMENTO RECEBIDO CORRESPONDE AO ENVIADO
+  if (incomingLength != incoming.length()) {
+    Serial.println("Erro: tamanho da mensagem não corresponde\n");
+    return;
+  }
+  // ------------------------------------------------------------------- EXIBE CONTEÚDO DA MENSAGEM
+  // Serial.println("Recebido de: 0x" + String(sender, HEX));
   // Serial.println("Enviado para: 0x" + String(recipient, HEX));
   // Serial.println("ID da mensagem: " + String(incomingMsgId));
-  Serial.println("Mensagem: " + incoming);
+  // Serial.println("Mensagem: " + incoming);
   // Serial.println("RSSI: " + String(LoRa.packetRssi()));
   // Serial.println("Snr: " + String(LoRa.packetSnr()));
-  Serial.println();
+  Serial.print("Recebido LoRa[0x" + String(sender, HEX) + "]: ");
+  Serial.println(incoming);
+
+  // Envia ao MQTT Feedback do Estado do Acionamento
+  feedback(incoming);
 }
 
-// -------------------------------------------------------------------------- CONFIGURA GPIO LED
-void setupLed(uint8_t LED, uint8_t STATUS) {
-  pinMode(LED, OUTPUT);
-  digitalWrite(LED, STATUS);
-}
-// -------------------------------------------------------------------------- PISCA O LED
-void ledBlink(uint8_t LED) {
-  digitalWrite(LED, !digitalRead(LED));
-  delay(200);
-  digitalWrite(LED, !digitalRead(LED));
-}
-// -------------------------------------------------------------------------- ALTERA ESTADO DO LED
-void ledToogle(uint8_t LED) {
-  digitalWrite(LED, !digitalRead(LED));
+// --------------------------------------------------------------------- ATUALIZA ESTADO NO DASHBOARD VIA MQTT
+void feedback(String message) {
+  String state;
+  String topic;
+  if (message.startsWith("LED:")) {
+    state = message.substring(4);
+    topic = topic_led_state;
+  } else if (message.startsWith("SWITCH1:")) {
+    state = message.substring(8);
+    topic = topic_switch1_state;
+  } else if (message.startsWith("STATE")) {
+    Serial.println(message);
+  }
+
+  if(!topic.isEmpty() && !state.isEmpty()) {
+    Serial.println("Enviado MQTT[" + topic + "]: " + state);
+    Serial.println();
+    client.publish(topic.c_str(), state.c_str());
+  }
 }
 
-// -------------------------------------------------------------------------- FUNÇÃO SETUP
+// --------------------------------------------------------------------- FUNÇÃO SETUP
 void setup() {
   Serial.begin(115200);                                               // Inicia comunicação Serial
   setupWiFi();                                                        // Configura Access Point ou Conecta à rede 
-  client.setServer(mqtt_server, mqtt_port);                           // Define endereço do servidor e porta
-  client.setCallback(callback);                                       // Define função callback
-  setupLed(LED_BUILTIN, HIGH);                                        // Define LED pin como saída
+  setupMQTT();                                                        // Define servidor MQTT a ser conectado e função callback ao receber mensagem
   setupLoRa(433E6);                                                   // Configura e inicia o LoRa (433 MHz ou 915 MHz)
-  LoRa.onReceive(onReceive);                                          // Define o que será realizado ao receber uma mensagem LoRa
   LoRa.receive();                                                     // Coloca o LoRa em modo receptor
-  Serial.println("\nESP8266 MASTER - iniciado com sucesso!");
+  Serial.println("\nESP8266 MASTER iniciado");
 }
 
-// -------------------------------------------------------------------------- FUNÇÃO LOOP
+// --------------------------------------------------------------------- FUNÇÃO LOOP
 void loop() {
-  // ------------------------------------------------------------------------ ESTABELECE CONEXÃO COM O SERVIDOR MQTT
+  // ------------------------------------------------------------------- ESTABELECE CONEXÃO COM O SERVIDOR MQTT
   if(!client.connected()) {
     connectMQTT();
   }
-  // ------------------------------------------------------------------------ MANTÉM INSTÂNCIA DO MQTT
+  // ------------------------------------------------------------------- MANTÉM INSTÂNCIA DO MQTT
   client.loop();
-  // ------------------------------------------------------------------------ SOLICITA AOS SLAVES O STATUS DOS DISPOSITIVOS
+  // ------------------------------------------------------------------- SOLICITA AOS SLAVES O STATUS DOS DISPOSITIVOS
   if (millis() - lastSendTime >= interval && !dataReceived) {
     Serial.println("Solicitando dados do ESP8266 Slave\n");
-    String message = "GET_STATE";
-    sendMessage(message, slave1Address);
-    LoRa.receive();                                                        // Volta para o modo receive
-    // ledBlink(LED_BUILTIN);
+    String message = "STATE:REQUEST";
+    sendMessage(message, slaveAddress);
     lastSendTime = millis();
   }
-  // ------------------------------------------------------------------------ EXECUTA AO RECEBER UMA MENSAGEM
+  // ------------------------------------------------------------------- EXECUTA AO RECEBER UMA MENSAGEM
   if (dataReceived) {
-    showData();
-    if(incoming == "LED STATE: 1") {
-      client.publish(topic_slave_led_state, "ON");
-    } else {
-      client.publish(topic_slave_led_state, "OFF");
-    }
+    receiveData();
     dataReceived = false;
   }
-  // ------------------------------------------------------------------------ 
-
 }
